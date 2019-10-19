00334b6e18a1e331cee4943783c31e261c5ac792
Andy Dai <jdai023@uottawa.ca>
2019-10-19 19:45:16 -0400

    add the code writen by song shifenfg in activity2

--- project/app/src/main/java/com/example/group23debug/Main2Activity.java
+++ project/app/src/main/java/com/example/group23debug/Main2Activity.java
@@ -3,12 +3,17 @@ package com.example.group23debug;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.content.Intent;
+import android.widget.CompoundButton;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Switch;
 import android.widget.EditText;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,15 +21,20 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
 
-public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
-    TextView textView;
+public class Main2Activity extends AppCompatActivity  {
+
+    private TextView textView;
+    private Switch swbutton;
+    private Button signbutton;
     private EditText firstNameEditText;
     private EditText lastNameEditText;
-    private EditText typeEditText;
+    private Switch typeEditText;
     private EditText emailEditText;
     private EditText passwordEditText;
     private EditText confirmPassEditText;
+
 
     FirebaseAuth mAuth;
     FirebaseDatabase mDatabase;
@@ -32,10 +42,51 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        swbutton = (Switch) findViewById(R.id.switch1) ;
+        firstNameEditText = (EditText)findViewById(R.id.editText);
+        lastNameEditText = (EditText)findViewById(R.id.editText2);
+        typeEditText =(Switch)findViewById(R.id.switch1);
+        emailEditText=(EditText)findViewById(R.id.editText4);
+        passwordEditText=(EditText)findViewById(R.id.editText5);
+        confirmPassEditText=(EditText)findViewById(R.id.editText6);
+
+        swbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //Todo
+
+                    Toast.makeText(Main2Activity.this,"Sign an employee account",Toast.LENGTH_SHORT).show();
+                }else{
+                    //Todo
+
+                    Toast.makeText(Main2Activity.this,"Sign a patient account",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+        signbutton=(Button)findViewById(R.id.button);
+        signbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(lastNameEditText.isEmpty(Username).toString()==null||
+
+                emailEditText.getText().toString()==null||
+                passwordEditText.getText().toString()==null||
+                confirmPassEditText.getText().toString()==null){
+                    Toast.makeText(Main2Activity.this,"Please fill out all questions.",Toast.LENGTH_SHORT).show();
+                }else{
+
+                }
+
+
+            }
+        });
 
         textView=findViewById(R.id.textView2);
         textView.setOnClickListener(new View.OnClickListener(){
@@ -53,8 +104,8 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
 
     }
-    @Override
-    public void onClick(View v){
+
+    public void SignUpOnClick(View v){
         String firstName = firstNameEditText.getText().toString();
         String lastName = lastNameEditText.getText().toString();
         String studentNo = typeEditText.getText().toString();
