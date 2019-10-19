6f3249199e62d08da10aa4e1629b3c80a04b1797
Andy Dai <jdai023@uottawa.ca>
2019-10-19 16:17:29 -0400

    add the UI for the sign up, log in and information

---
+++ project/app/src/main/java/com/example/group23debug/Main2Activity.java
@@ -0,0 +1,82 @@
+package com.example.group23debug;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+    TextView textView;
+    private EditText firstNameEditText;
+    private EditText lastNameEditText;
+    private EditText studentEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText confirmPassEditText;
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView=findViewById(R.id.textView2);
+        textView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
+
+                startActivityForResult(intent,0x01);
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+
+
+    }
+    @Override
+    public void onClick(View v){
+        String firstName = firstNameEditText.getText().toString();
+        String lastName = lastNameEditText.getText().toString();
+        String studentNo = studentEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String password =passwordEditText.getText().toString();
+
+        final Student mStudent = new Student(firstName, lastName, studentNo, email, password);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    finish();
+                    startActivity(
+                            new Intent(getApplicationContext(), Main3Activity.class)
+                    );
+                }else{
+                    Toast.makeText(Main2Activity.this,"Firebase Authentivation Error",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+
+
+
+}
--- project/app/src/main/java/com/example/group23debug/MainActivity.java
+++ project/app/src/main/java/com/example/group23debug/MainActivity.java
@@ -2,15 +2,60 @@ package com.example.group23debug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private EditText editText;
+    private CheckBox checkBox;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.notamem);
+        textView.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivityForResult(intent,0x01);
+            }
+        });  // go to Main2 Activity
+
+        button = findViewById(R.id.buttonlogin);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+                startActivityForResult(intent,0001);
+            }
+        });
+        editText = (EditText)findViewById(R.id.passwordid);
+        checkBox=(CheckBox)findViewById(R.id.showpassword);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    editText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }else{
+                    editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });// show or hide password
+
+
     }
+
+
 }
 
 
