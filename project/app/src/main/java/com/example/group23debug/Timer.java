b8ab7fbc42d4219cd20483a67096d065d8f6dae5
Andy Dai <jdai023@uottawa.ca>
2019-11-20 19:20:49 -0500

    Time error fixed, ending time cannot be set before starting time.

--- project/app/src/main/java/com/example/group23debug/Timer.java
+++ project/app/src/main/java/com/example/group23debug/Timer.java
@@ -25,13 +25,13 @@ import java.util.Calendar;
 
 public class Timer extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     Boolean click = false;
-    Button btnTimer,btntimerend;
-    TextView texTimer,texTimerend;
+    Button btnTimer, btntimerend;
+    TextView texTimer, texTimerend;
 
-    int day,hour,minute,month,year;
-    int day_x,hour_x,minute_x,month_x,year_x;
+    int day, hour, minute, month, year;
+    int day_x, hour_x, minute_x, month_x, year_x;
 
-    int month1,day1,hour1,month2,day2,hour2;
+    int month1, day1, hour1, month2, day2, hour2;
 
     String name;
     DatabaseReference mRef;
@@ -41,95 +41,102 @@ public class Timer extends AppCompatActivity implements DatePickerDialog.OnDateS
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        btntimerend  =(Button) findViewById(R.id.btntimerend);
+        btntimerend = (Button) findViewById(R.id.btntimerend);
         btnTimer = findViewById(R.id.btntimer);
-        texTimer= findViewById(R.id.texTimer);
+        texTimer = findViewById(R.id.texTimer);
         texTimerend = findViewById(R.id.texTimerend);
         name = getIntent().getStringExtra("name");
-        month1=day1=hour1=month2=day2 =hour2 =0;
+        month1 = day1 = hour1 = month2 = day2 = hour2 = 0;
 
 
         mRef = FirebaseDatabase.getInstance().getReference().child("Clinic").child(name).child("Time");
         btntimerend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                timerInterface time = new timerInterface(month1,day1,hour1);
+                timerInterface time = new timerInterface(month1, day1, hour1);
                 mRef.child("Start").setValue(time).addOnCompleteListener(Timer.this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Timer.this,"Add service successful.",Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Timer.this, "Add service successful.", Toast.LENGTH_SHORT).show();
                             finish();
-                        }else{
-                            Toast.makeText(Timer.this,"Firebase Database Error",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Timer.this, "Firebase Database Error", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
-                timerInterface time2 = new timerInterface(month2,day2,hour2);
+                timerInterface time2 = new timerInterface(month2, day2, hour2);
                 mRef.child("End").setValue(time2).addOnCompleteListener(Timer.this, new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Timer.this,"Add service successful.",Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Timer.this, "Add service successful.", Toast.LENGTH_SHORT).show();
                             finish();
-                        }else{
-                            Toast.makeText(Timer.this,"Firebase Database Error",Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Timer.this, "Firebase Database Error", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
             }
         });
-
 
 
         btnTimer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Calendar c = Calendar.getInstance();
-                year =c.get(Calendar.YEAR);
-                month =c.get(Calendar.MONTH);
-                day =c.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog datePickerDialog = new DatePickerDialog(Timer.this,Timer.this,year,month,day);
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Timer.this, Timer.this, year, month, day);
                 datePickerDialog.show();
             }
         });
-
 
 
     }
 
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        year_x =year;
-        month_x=month;
+        year_x = year;
+        month_x = month;
         day_x = dayOfMonth;
         Calendar c = Calendar.getInstance();
         hour = c.get(Calendar.HOUR);
         minute = c.get(Calendar.MINUTE);
-        TimePickerDialog timepickerdialog = new TimePickerDialog(Timer.this,Timer.this,
-                hour,minute,true);
+        TimePickerDialog timepickerdialog = new TimePickerDialog(Timer.this, Timer.this,
+                hour, minute, true);
         timepickerdialog.show();
     }
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
-        hour_x= hourOfDay;
-        minute_x=minute;
-        if(click==false){
-            texTimer.setText("Month: "+month_x+"\n"+"Day: "+ day_x +"\n"+"hour: "+hour_x);
+        hour_x = hourOfDay;
+        minute_x = minute;
+        if (click == false) {
+            texTimer.setText("Month: " + month_x + "\n" + "Day: " + day_x + "\n" + "hour: " + hour_x);
             month1 = month_x;
             day1 = day_x;
             hour1 = hour_x;
             click = true;
-        }else{
-            texTimerend.setText("Month: "+month_x+"\n"+"Day: "+ day_x +"\n"+"hour: "+hour_x);
-            month2 = month_x;
-            day2 = day_x;
-            hour2 = hour_x;
-            click = false;
-        }
+        } else {
+            texTimerend.setText("Month: " + month_x + "\n" + "Day: " + day_x + "\n" + "hour: " + hour_x);
 
+            if (month1 > month_x || day1 > day_x || hour1 > hour_x) {
+                Toast.makeText(Timer.this, "Invalid Time!", Toast.LENGTH_SHORT).show();
+                month_x=0;
+                day_x=0;
+                hour_x=0;
+                texTimerend.setText("Month: " + month_x + "\n" + "Day: " + day_x + "\n" + "hour: " + hour_x);
+            } else {
+                month2 = month_x;
+                day2 = day_x;
+                hour2 = hour_x;
+                click = false;
+            }
+
+        }
     }
 }
