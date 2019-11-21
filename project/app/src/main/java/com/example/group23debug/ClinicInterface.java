1969af2de8b117af5cf461035e36ffc53530ede4
Aozhuo Zhang <azhan089@uottawa.ca>
2019-11-21 03:17:17 -0500

    Add working times check for staff,change few bugs

---
+++ project/app/src/main/java/com/example/group23debug/ClinicInterface.java
@@ -0,0 +1,4 @@
+package com.example.group23debug;
+
+public class ClinicInterface {
+}
---
+++ project/app/src/main/java/com/example/group23debug/Timer.java
@@ -0,0 +1,14 @@
+package com.example.group23debug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
