f5f1be4c4e1ff801fb90d3a821f3d65f2e322e7b
Andy Dai <jdai023@uottawa.ca>
2019-11-20 22:44:08 -0500

    add the test file for welcomeStaff

---
+++ project/app/src/androidTest/java/com/example/group23debug/welcomeEmployeeTest.java
@@ -0,0 +1,41 @@
+package com.example.group23debug;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertNotNull;
+
+public class welcomeEmployeeTest {
+    @Rule
+    public ActivityTestRule<welcom_staff> mActivityTestRule = new ActivityTestRule<welcom_staff>(welcom_staff.class);
+    private welcom_staff mActivity = null;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void LaunchSecondActivity() throws Exception {
+        assertNotNull(mActivity.findViewById(R.id.staffname));
+        assertNotNull(mActivity.findViewById(R.id.staffsype));
+        assertNotNull(mActivity.findViewById(R.id.stafffname));
+        assertNotNull(mActivity.findViewById(R.id.stafflname));
+        assertNotNull(mActivity.findViewById(R.id.staffemail));
+        assertNotNull(mActivity.findViewById(R.id.staffclinicbtn));
+        assertNotNull(mActivity.findViewById(R.id.stafflogout));
+
+
+    }
+}
