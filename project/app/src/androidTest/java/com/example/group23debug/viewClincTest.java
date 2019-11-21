d63491bb1ef518b48032553b08f7ec19dc4dcf36
Andy Dai <jdai023@uottawa.ca>
2019-11-20 21:09:06 -0500

    add the test filr for Viewclinc for the employee

--- project/app/src/androidTest/java/com/example/group23debug/viewClincTest.java
+++ project/app/src/androidTest/java/com/example/group23debug/viewClincTest.java
@@ -1,8 +1,7 @@
 package com.example.group23debug;
+
 import android.app.Activity;
 import android.app.Instrumentation;
-
-
 
 import androidx.test.rule.ActivityTestRule;
 
@@ -14,30 +13,30 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
-
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.junit.Assert.assertNotNull;
-public class LoginpagejumpTest {
+
+public class viewClincTest {
     @Rule
-    public ActivityTestRule<Login> mActivityTestRule= new ActivityTestRule<Login>(Login.class);
-    private Login mActivity=null;
-    private Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(welcomAdm.class.getName(),null,false);
+    public ActivityTestRule<viewclinicemp> mActivityTestRule = new ActivityTestRule<viewclinicemp>(viewclinicemp.class);
+    private viewclinicemp mActivity = null;
+    private Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(addclinic.class.getName(), null, false);
+
     @Before
     public void setUp() throws Exception {
-        mActivity=mActivityTestRule.getActivity();
+        mActivity = mActivityTestRule.getActivity();
     }
+
     @Test
     public void LaunchSecondActivity() throws Exception {
-        assertNotNull(mActivity.findViewById(R.id.buttonlogin));
-        onView(withId(R.id.emailid)).perform(typeText("admin"), closeSoftKeyboard());
-        onView(withId(R.id.passwordid)).perform(typeText("5T5ptQ"), closeSoftKeyboard());
-
-        onView(withId(R.id.buttonlogin)).perform(click());
-        Activity secondActivity=getInstrumentation().waitForMonitorWithTimeout(monitor, 8000);
+        assertNotNull(mActivity.findViewById(R.id.addclinicbtn));
+        assertNotNull(mActivity.findViewById(R.id.cliniclistView));
+        onView(withId(R.id.addclinicbtn)).perform(click());
+        Activity secondActivity = getInstrumentation().waitForMonitorWithTimeout(monitor, 8000);
         assertNotNull(secondActivity);
         secondActivity.finish();
 
     }
 }
+
