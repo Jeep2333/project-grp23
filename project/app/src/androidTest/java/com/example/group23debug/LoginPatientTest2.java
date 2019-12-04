d294f2fd5f22da315ddd40d800106856587f95a1
Andy Dai <jdai023@uottawa.ca>
2019-12-03 20:28:41 -0500

    3 test classese added

---
+++ project/app/src/androidTest/java/com/example/group23debug/LoginPatientTest2.java
@@ -0,0 +1,41 @@
+package com.example.group23debug;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class LoginPatientTest2 {
+    @Rule
+    public ActivityTestRule<Login> mActivityTestRule= new ActivityTestRule<Login>(Login.class);
+    private Login mActivity=null;
+    private Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(welcomenor.class.getName(),null,false);
+    @Before
+    public void setUp() throws Exception {
+        mActivity=mActivityTestRule.getActivity();
+    }
+    @Test
+    public void LaunchSecondActivity() throws Exception {
+        assertNotNull(mActivity.findViewById(R.id.buttonlogin));
+        onView(withId(R.id.emailid)).perform(typeText("303"), closeSoftKeyboard());
+        onView(withId(R.id.passwordid)).perform(typeText("rua"), closeSoftKeyboard());
+
+        onView(withId(R.id.buttonlogin)).perform(click());
+        Activity secondActivity=getInstrumentation().waitForMonitorWithTimeout(monitor, 8000);
+        assertEquals(null,secondActivity);
+
+    }
+}
